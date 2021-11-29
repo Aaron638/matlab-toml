1cd82c3c57d01d82a549d22f647ba32ce40786a2
asmartin <asmartin@iastate.edu>
2021-11-29 13:13:00 -0600

    Re-arranged parsevalue to prioritize string, stringliteral, arrays, and tables before other types.

--- +toml/private/parsevalue.m
+++ +toml/private/parsevalue.m
@@ -35,126 +35,7 @@ function val = parsevalue(str, force)
   % trim space on each side
   trimmed_val = strtrim(val);
 
-%% datetimes
-
-  % make regexes
-  is_match = @(s, p) ~isempty(regexp(s, p, 'ONCE'));
-  date_regexp = '\d{4}-\d{2}-\d{2}';
-  upto24 = ['(', strjoin( ...
-      arrayfun(@(elem) sprintf('%02d', elem), 0:23, 'uniformoutput', false), ...
-      '|'), ')'];
-  fract_sec = '\.\d{1,9}';
-  time_regexp = [upto24, ':[0-6]\d:[0-6]\d(', fract_sec, ')?'];
-  offset_regexp = ['(Z|[-+]', upto24, ':[0-6]\d(:[0-6]\d)?)$'];
-
-  % see what fits
-  has_date = is_match(trimmed_val, date_regexp);
-  has_time = is_match(trimmed_val, time_regexp);
-  is_datetime = has_date && has_time;
-  is_datetime_t = is_datetime && ...
-      is_match(trimmed_val, [date_regexp, 'T', time_regexp]);
-  has_fr_sec = has_time && is_match(trimmed_val, fract_sec);
-  has_offset = has_time && is_match(trimmed_val, offset_regexp);
-
-  % make formats
-  date_fmt = 'yyyy-MM-dd';
-  time_fmt = 'HH:mm:ss';
-  fract_sec_fmt = '.SSSSSSSSS';
-
-  % do what we can with it
-  if is_datetime
-    dtargs = {};
-    if is_datetime_t
-      fmt_str = [date_fmt, '''T''', time_fmt];
-    else
-      fmt_str = [date_fmt, ' ', time_fmt];
-    end
-    if has_fr_sec
-      fmt_str = [fmt_str, fract_sec_fmt];
-    end
-    if has_offset
-      fmt_str = [fmt_str, 'Z'];
-      dtargs = {'TimeZone', 'UTC'};
-    end
-    val = datetime(trimmed_val, 'InputFormat', fmt_str, dtargs{:});
-    return
-  elseif has_date
-    fmt_str = date_fmt;
-    val = datetime(trimmed_val, 'InputFormat', fmt_str);
-    return
-  elseif has_time
-    fmt_str = time_fmt;
-    if has_fr_sec
-      fmt_str = [fmt_str, fract_sec_fmt];
-    end
-    val = datetime(trimmed_val, 'InputFormat', fmt_str);
-    return
-  end
-
-%% check for numeric types
-
-  % utils for integers
-  is_int = @(t, s, c) all(ismember(t, [s, '_'])) && ...
-           length(t) > 3 && isequal(t(1:2), ['0', c]);
-  descore = @(t) strrep(t(3:end), '_', '');
-  specs.bin = 'b01';
-  specs.oct = 'o01234567';
-  specs.hex = 'x0123456789abcdefABCDEF';
-
-  dec_num = '([0-9][_0-9]*)?[0-9]';
-  dec_int = ['[+-]?', dec_num];
-  specs.dec = [ ...
-      '^', dec_int ...            % integer part
-      '(\.', dec_num, ')?' ...    % fractional part
-      '([eE]', dec_int, ')?$' ... % exponential part
-              ];
-
-  % binary
-  if is_int(trimmed_val, specs.bin, 'b')
-    val = bin2dec(descore(trimmed_val));
-    return
-  % octal
-  elseif is_int(trimmed_val, specs.oct, 'o')
-    val = base2dec(descore(trimmed_val), 8);
-    return
-  % hexadecimal
-  elseif is_int(trimmed_val, specs.hex, 'x')
-    val = hex2dec(descore(trimmed_val));
-    return
-  % decimal (including floats)
-  elseif ~isempty(regexp(trimmed_val, specs.dec, 'ONCE'))
-    val = str2double(strrep(val, '_', ''));
-    % error for using leading zeros on a decimal integer
-    if isfinite(val) && ~mod(val, 1) && val && trimmed_val(1) == '0'
-      error('toml:DecIntLeadingZeros', ...
-            'Decimal integers may not have leading zeros.')
-    end
-    return
-  end
-
-  % special values of float
-  spec_flt = {'inf'; 'nan'};
-  op_chars = {'', '+', '-'};
-  spec_cmp = reshape( ...
-      strcat(repmat(op_chars, 2, 1), repmat(spec_flt, 1, 3)), ...
-                     [], 1);
-  if any(strcmp(trimmed_val, spec_cmp))
-    val = str2double(val);
-    return
-  elseif any(strcmpi(trimmed_val, spec_cmp))
-    error('toml:UppercaseSpecialFloat', ...
-          'Special floating-point values must be lowercase.')
-  end
-
-%% booleans
-  if any(strcmp(trimmed_val, {'true', 'false'}))
-    val = ~mod(numel(trimmed_val), 2);
-    return
-  elseif any(strcmpi(trimmed_val, {'true', 'false'}))
-    error('toml:UppercaseBoolean', ...
-          'Boolean values must be lowercase.')
-  end
-
+%%% Check for string, string literal, array, or table before other datatypes
 %% strings
   % basic strings
   if trimmed_val(1) == '"'
@@ -351,6 +232,126 @@ function val = parsevalue(str, force)
     end
 
     return
+  end
+
+%% datetimes
+
+  % make regexes
+  is_match = @(s, p) ~isempty(regexp(s, p, 'ONCE'));
+  date_regexp = '\d{4}-\d{2}-\d{2}';
+  upto24 = ['(', strjoin( ...
+      arrayfun(@(elem) sprintf('%02d', elem), 0:23, 'uniformoutput', false), ...
+      '|'), ')'];
+  fract_sec = '\.\d{1,9}';
+  time_regexp = [upto24, ':[0-6]\d:[0-6]\d(', fract_sec, ')?'];
+  offset_regexp = ['(Z|[-+]', upto24, ':[0-6]\d(:[0-6]\d)?)$'];
+
+  % see what fits
+  has_date = is_match(trimmed_val, date_regexp);
+  has_time = is_match(trimmed_val, time_regexp);
+  is_datetime = has_date && has_time;
+  is_datetime_t = is_datetime && ...
+      is_match(trimmed_val, [date_regexp, 'T', time_regexp]);
+  has_fr_sec = has_time && is_match(trimmed_val, fract_sec);
+  has_offset = has_time && is_match(trimmed_val, offset_regexp);
+
+  % make formats
+  date_fmt = 'yyyy-MM-dd';
+  time_fmt = 'HH:mm:ss';
+  fract_sec_fmt = '.SSSSSSSSS';
+
+  % do what we can with it
+  if is_datetime
+    dtargs = {};
+    if is_datetime_t
+      fmt_str = [date_fmt, '''T''', time_fmt];
+    else
+      fmt_str = [date_fmt, ' ', time_fmt];
+    end
+    if has_fr_sec
+      fmt_str = [fmt_str, fract_sec_fmt];
+    end
+    if has_offset
+      fmt_str = [fmt_str, 'Z'];
+      dtargs = {'TimeZone', 'UTC'};
+    end
+    val = datetime(trimmed_val, 'InputFormat', fmt_str, dtargs{:});
+    return
+  elseif has_date
+    fmt_str = date_fmt;
+    val = datetime(trimmed_val, 'InputFormat', fmt_str);
+    return
+  elseif has_time
+    fmt_str = time_fmt;
+    if has_fr_sec
+      fmt_str = [fmt_str, fract_sec_fmt];
+    end
+    val = datetime(trimmed_val, 'InputFormat', fmt_str);
+    return
+  end
+
+%% check for numeric types
+
+  % utils for integers
+  is_int = @(t, s, c) all(ismember(t, [s, '_'])) && ...
+           length(t) > 3 && isequal(t(1:2), ['0', c]);
+  descore = @(t) strrep(t(3:end), '_', '');
+  specs.bin = 'b01';
+  specs.oct = 'o01234567';
+  specs.hex = 'x0123456789abcdefABCDEF';
+
+  dec_num = '([0-9][_0-9]*)?[0-9]';
+  dec_int = ['[+-]?', dec_num];
+  specs.dec = [ ...
+      '^', dec_int ...            % integer part
+      '(\.', dec_num, ')?' ...    % fractional part
+      '([eE]', dec_int, ')?$' ... % exponential part
+              ];
+
+  % binary
+  if is_int(trimmed_val, specs.bin, 'b')
+    val = bin2dec(descore(trimmed_val));
+    return
+  % octal
+  elseif is_int(trimmed_val, specs.oct, 'o')
+    val = base2dec(descore(trimmed_val), 8);
+    return
+  % hexadecimal
+  elseif is_int(trimmed_val, specs.hex, 'x')
+    val = hex2dec(descore(trimmed_val));
+    return
+  % decimal (including floats)
+  elseif ~isempty(regexp(trimmed_val, specs.dec, 'ONCE'))
+    val = str2double(strrep(val, '_', ''));
+    % error for using leading zeros on a decimal integer
+    if isfinite(val) && ~mod(val, 1) && val && trimmed_val(1) == '0'
+      error('toml:DecIntLeadingZeros', ...
+            'Decimal integers may not have leading zeros.')
+    end
+    return
+  end
+
+  % special values of float
+  spec_flt = {'inf'; 'nan'};
+  op_chars = {'', '+', '-'};
+  spec_cmp = reshape( ...
+      strcat(repmat(op_chars, 2, 1), repmat(spec_flt, 1, 3)), ...
+                     [], 1);
+  if any(strcmp(trimmed_val, spec_cmp))
+    val = str2double(val);
+    return
+  elseif any(strcmpi(trimmed_val, spec_cmp))
+    error('toml:UppercaseSpecialFloat', ...
+          'Special floating-point values must be lowercase.')
+  end
+
+%% booleans
+  if any(strcmp(trimmed_val, {'true', 'false'}))
+    val = ~mod(numel(trimmed_val), 2);
+    return
+  elseif any(strcmpi(trimmed_val, {'true', 'false'}))
+    error('toml:UppercaseBoolean', ...
+          'Boolean values must be lowercase.')
   end
 
 %% invalid datatype?
